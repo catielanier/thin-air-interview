12d07a398fbd1d356b76a37dae0a126d19e851ce
Catie Lanier <hello@catielanier.ca>
2024-12-08 16:32:32 -0500

    add comments

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,26 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import { onMount } from "svelte";
+  import axios from 'axios';
+  import type { Cart, Items } from "./utils/types";
+  import { getStorageItem } from "./utils/storage";
+
+  // Set initial states
+  let items: Items = [];
+  // Cart and subtotal can be derived from localStorage to be maintained across browsing sessions, will default to blank if never created
+  // Must parse JSON for cart and integer for subtotal since all localStorage is stringified
+  let cart: Cart = JSON.parse(getStorageItem('cart')!) ?? [];
+  let subtotal = parseInt(getStorageItem('subtotal')!, 10) ?? 0;
+
+  // Retrieve sale items from backend when app is mounted
+  onMount((): void => {
+    axios.get('/api/v1/items').then(({ data }): void => {
+      items = data;
+    });
+  });
 </script>
 
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <h1>Holy Shirt!</h1>
 </main>
 
 <style>
