296429363b9f526eff472039b7c34794ed5119ec
Catie Lanier <hello@catielanier.ca>
2024-12-08 17:47:08 -0500

    add comments

--- src/App.svelte
+++ src/App.svelte
@@ -9,7 +9,8 @@
   // Set initial states
   let items: Items = [];
   // Cart and subtotal can be derived from localStorage to be maintained across browsing sessions, will default to blank if never created
-  // Must parse JSON for cart and integer for subtotal since all localStorage is stringified
+  // Must parse JSON for cart and float for subtotal since all localStorage is stringified
+  // Float required since we won't always have whole numbers
   let cart: Cart = JSON.parse(getStorageItem('cart')!) ?? [];
   let subtotal = parseFloat(getStorageItem('subtotal')!) ?? 0;
   let cartIsUpdating: boolean = false;
@@ -21,6 +22,7 @@
     });
   });
 
+  // Standalone function to ensure cart storage is updated
   const updateCartStorage = (): void => {
     setStorageItem('cart', JSON.stringify(cart));
     setStorageItem('subtotal', subtotal.toString());
@@ -46,21 +48,31 @@
 
 <main>
   <h1>Holy Shirt!</h1>
+  <!-- Wait for products to be retrieved before rendering -->
   {#if items.length}
     <div class="items">
+      <!-- Loop through each product and render with child component -->
       {#each items as item}
         <Item {item} {updateCart} {cartIsUpdating} />
       {/each}
     </div>
   {/if}
+  <!-- Check to see if items are in the cart before rendering -->
   {#if cart.length}
     <div class="cart">
       <h2>Your Cart:</h2>
       <div class="cart-items">
+        <!-- Loop through each cart item and render with child component -->
         {#each cart as cartItem}
           <CartItem item={cartItem} itemName={items.find(item => item.id === cartItem.id)?.description} />
         {/each}
       </div>
+      <!--
+       Don't need to pass subtotal to child, just render directly here,
+       if we were doing Sales Taxes, we would pass to child to handle
+       rendering each with less nesting
+      -->
+      <!-- TODO: Create child component to show subtotal, taxes, and grand total -->
       <p>Subtotal: ${subtotal.toFixed(2)}</p>
     </div>
   {/if}
