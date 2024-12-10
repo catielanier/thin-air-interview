afa0cb8cd47bee6faeb335068466b3c5ac6a8a0d
Catie Lanier <hello@catielanier.ca>
2024-12-09 19:28:22 -0500

    fix preprocessing css with unit tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import preprocess from 'svelte-preprocess'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [svelte()],
+  plugins: [svelte({
+    preprocess: preprocess({
+      postcss: true,
+    })
+  })],
   server: {
     proxy: {
       '/api': {
